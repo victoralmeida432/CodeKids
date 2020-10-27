d3ff6d5d6ae8f7414ac819423438b22491088e3c
Victor <vala@cesar.school>
2020-10-27 13:00:34 -0300

    Sprout + Persongem + assets

---
+++ Sprite.js
@@ -0,0 +1,14 @@
+function Sprite(x, y, largura, altura){
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+    this.desenha = function(xCanvas, yCanvas){
+        context.drawImage(img, this.x,this.y, this.largura, this.altura, xCanvas, yCanvas);
+
+    }
+
+}
+
+var bg = new Sprite(0, 0, 0, 0);
+var spritePersonagem = new Sprite(0,0,50,70);
